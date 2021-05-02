5bfb255f8d2c49ab051aef5cd344f936df1abce3
tusar30 <78675083+tusar30@users.noreply.github.com>
2021-05-02 06:40:00 +0600

    All Flies

---
+++ Test Interface_Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mphone r1 = new Radio("FM Radio", 89.06);
+            Mphone r2 = new Radio("FM Radio", 101.01);
+            r1.play(true);
+            r1.playnxt();
+            Console.WriteLine();
+            r2.play(true);
+            r2.playpre();
+            Console.WriteLine();
+
+            Songs m1 = new Songs("Never Lie to me", "Benson", 2019, 200);
+            Songs m2 = new Songs("Hello", "Lionel Richie", 2020, 280);
+            Songs m3 = new Songs("Oniket Prantor", "Artcell", 2016, 307);
+            Mphone ph = new Player("Music Player", m1);
+            ph.add(m1, m2, m3);
+            ph.play(true);
+            ph.playnxt();
+            Console.WriteLine();
+            ph.playnxt();
+            Console.WriteLine();
+            ph.playnxt();
+            Console.WriteLine();
+            ph.playpre();
+        }
+    }
+}
---
+++ Test Interface_Lab2/Radio.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_Lab2
+{
+    class Radio : Mphone, RadioIF, PlayerIF
+    {
+        public double channel { get; set; }
+        public string player { get; set; }
+
+        public Radio(string player, double channel)
+        {
+            this.player = player;
+            this.channel = channel;
+        }
+
+
+        public void change()
+        {
+        }
+
+        public override void play(bool on)
+        {
+            Console.WriteLine("Radio Playing..!!..");
+            showInfo();
+        }
+
+        public override void playnxt()
+        {
+            if (channel <= 105.89)
+            {
+                channel += 0.10;
+            }
+            else Console.WriteLine("Can't going next Channel!");
+            showInfo();
+        }
+
+        public override void playpre()
+        {
+            if (channel >= 0.10)
+            {
+                channel -= 0.10;
+            }
+            else Console.WriteLine("Can't going previous Channel!");
+            showInfo();
+        }
+        public void showInfo()
+        {
+            Console.WriteLine("Channel : " + channel);
+        }
+
+        public void reTune(double frequency)
+        {
+
+        }
+        public override void Switch(bool on)
+        {
+            Console.WriteLine("Radio Playing..!!..");
+            showInfo();
+        }
+        public override void setVolume(int loudness)
+        {
+            Console.WriteLine("Volume : " + loudness);
+        }
+    }
+}
