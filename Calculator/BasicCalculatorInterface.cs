4049e691e7f5e59e24f79eb46763b7270b1bcf2c
tusar30 <78675083+tusar30@users.noreply.github.com>
2021-05-02 06:25:52 +0600

    Final_Calc

---
+++ Calculator/BasicCalculatorInterface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class BasicCalculatorInterface : BasicAndScientificInterFace
+    {
+        public void sum(int a, int b)
+        {
+            Console.WriteLine("The sum is : " + (a + b));
+        }
+        public void sub(int a, int b)
+        {
+            Console.WriteLine("The sub is : " + (a - b));
+        }
+        public void multiplication(int a, int b)
+        {
+            Console.WriteLine("The multiplication is : " + (a * b));
+        }
+        public void division(double a, double b)
+        {
+            Console.WriteLine("The division is : " + (a / b));
+        }
+
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicCalculatorInterface c1 = new BasicCalculatorInterface();
+            c1.sum(10, 20);
+            c1.sub(100, 12);
+            c1.multiplication(102, 1234);
+            c1.division(12345, 7);
+
+            ScientificCalculatorInterface c2 = new ScientificCalculatorInterface();
+            c2.sum(34, 54);
+            c2.sub(10, 5);
+            c2.multiplication(123, 456);
+            c2.division(123, 6);
+            c2.XtoY(5, 3);
+            c2.Sqroot(103);
+            c2.cuberoot(12345);
+        }
+    }
+}
