d063cf5e32c8d788a9cef713da0aaad6cf4fda18
tusar30 <78675083+tusar30@users.noreply.github.com>
2021-05-02 07:00:39 +0600

    Test Interface 1 & 2

---
+++ Calculator/BasicAndScientificInterFace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    interface BasicAndScientificInterFace
+    {
+        void sum(int a, int b);
+        void sub(int a, int b);
+        void multiplication(int a, int b);
+        void division(double a, double b);
+
+
+    }
+}
