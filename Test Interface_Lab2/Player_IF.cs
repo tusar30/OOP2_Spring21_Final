ebdc1eae5b4d79692ee04cdad257955b81060a5d
tusar30 <78675083+tusar30@users.noreply.github.com>
2021-05-02 07:01:26 +0600

    Lab 2

---
+++ Test Interface_Lab2/Player_IF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_Lab2
+{
+    interface PlayerIF
+    {
+        void play(bool on);
+        void Switch(bool on);
+        void setVolume(int loudness);
+    }
+}
