a42c06897ae0d4caa8715db4764b30ed75499cf7
tusar30 <78675083+tusar30@users.noreply.github.com>
2021-05-02 06:16:27 +0600

    Test Interface
    Direct Uploaded. Gitbash not working properly. PC problem

---
+++ Bank_Interface/Bank_IF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Interface
+{
+    interface Bank_IF
+    {
+        bool Deposit(decimal amount);
+        bool Withdraw(decimal amount);
+    }
+}
